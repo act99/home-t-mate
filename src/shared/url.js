453024fbc0eeb1bfae53ad246807af44e2ae0200
act99 <dorxm999@gmail.com>
2022-03-08 18:56:39 +0900

    gogossing

--- src/shared/url.js
+++ src/shared/url.js
@@ -1,12 +1,11 @@
 // ** 테스트
-const BASE_URL = "http://15.165.42.20:8080";
-const REDIRECT_URI = "http://localhost:3000/user/kakao/callback";
-const WEB_SOCKET = "http://15.165.42.20:8080/ws-stomp";
+// const BASE_URL = "http://15.165.42.20:8080";
+// const REDIRECT_URI = "http://localhost:3000/user/kakao/callback";
+// const WEB_SOCKET = "http://15.165.42.20:8080/ws-stomp";
 // ** 배포
-// const BASE_URL = "https://goonzu.shop";
-// const REDIRECT_URI =
-//   "https://chat.d8pzcrluuw660.amplifyapp.com/user/kakao/callback";
-// const WEB_SOCKET = "https://goonzu.shop/wss-stomp";
+const BASE_URL = "https://goonzu.shop";
+const REDIRECT_URI = "https://develop.d1vuiyyszq8cy0.amplifyapp.com/";
+const WEB_SOCKET = "https://goonzu.shop/wss-stomp";
 
 const url = {
   BASE_URL,
