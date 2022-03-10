5e5c37ffd114b2715859e893e2211b04d95e02ad
act99 <dorxm999@gmail.com>
2022-03-11 00:57:22 +0900

    go

--- src/containers/VideoContainer.js
+++ src/containers/VideoContainer.js
@@ -15,7 +15,7 @@ import {
 import YoutubeVideo from "../components/YoutubeVideo";
 import ChatRoomSetting from "../components/ChatRoomSetting";
 // const OPENVIDU_SERVER_URL = "https://" + window.location.hostname + ":4443";
-const OPENVIDU_SERVER_URL = "https://hanghae99.shop:5443";
+const OPENVIDU_SERVER_URL = "https://hanghae99.shop:5000";
 const OPENVIDU_SERVER_SECRET = "MY_SECRET";
 
 class VideoContainer extends Component {
