a65d73471d38c936946db69adc5037c52cb1dceb
act99 <dorxm999@gmail.com>
2022-03-12 00:41:28 +0900

    채팅 합치기 완료 테스트

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -4,28 +4,40 @@ import {
   Divider,
   Fab,
   Grid,
+  IconButton,
   List,
   ListItem,
   ListItemText,
   TextField,
 } from "@mui/material";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import useStyle from "../styles/chattingStyle";
 import SendIcon from "@mui/icons-material/Send";
 import { useLocation } from "react-router-dom";
+import SockJS from "sockjs-client";
+import url from "../shared/url";
+import Stomp from "stompjs";
+import { sendingMessage } from "../shared/SocketFunc";
+import { actionCreators as chatActions } from "../redux/modules/chatReducer";
+const tokenCheck = document.cookie;
+const token = tokenCheck.split("=")[1];
 
 const ChatContainer = () => {
+  const dispatch = useDispatch();
   const classes = useStyle.makeChattingStyle();
   const chattingList = useSelector((state) => state.chatReducer.list);
-  const user = useSelector((state) => state.userReducer.user);
-  const nickname = user.nickname;
 
+  // ** params 로 받은 roomId 와 roomName
   const location = useLocation();
   const locationState = location.state;
   const roomName = locationState.roomName;
   const roomId = locationState.roomId;
   console.log(roomName, roomId);
+
+  // ** user 정보
+  const user = useSelector((state) => state.userReducer.user);
+  const nickname = user.nickname;
 
   // ** 메시지 핸들러
   const [sendMessage, setSendMessage] = React.useState({
@@ -34,6 +46,51 @@ const ChatContainer = () => {
     sender: "",
     message: "",
   });
+
+  // ** SockJS 설정
+  let options = {
+    debug: true,
+    header: { Authorization: token },
+    protocols: Stomp.VERSIONS.supportedVersions(),
+  };
+  const sock = new SockJS(url.WEB_SOCKET);
+  const ws = Stomp.over(sock, options);
+
+  // ** ws Open
+
+  const created = () => {
+    try {
+      ws.connect(
+        { Authorization: token },
+        (frame) => {
+          console.log("hi");
+          ws.subscribe(
+            `/sub/chat/room/${roomId}`,
+            (message) => {
+              let recv = JSON.parse(message.body);
+              dispatch(chatActions.getChat(recv));
+              chattingRef.current.scrollIntoView({ behavior: "smooth" });
+            },
+            { Authorization: token }
+          );
+        },
+        (error) => {
+          console.log("서버연결 실패", error);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const disconnected = () => {
+    if (ws !== null) {
+      ws.disconnect();
+      console.log("연결 종료");
+    }
+  };
+
+  // ** input값 핸들러
   const sendingMessageHandler = (event) => {
     setSendMessage({ ...sendMessage, message: event.target.value });
   };
@@ -42,7 +99,11 @@ const ChatContainer = () => {
 
   React.useEffect(() => {
     chattingRef.current.scrollIntoView({ behavior: "smooth" });
-    return () => {};
+    setSendMessage({ ...sendMessage, roomId: roomId, sender: nickname });
+    created();
+    return () => {
+      disconnected();
+    };
   }, []);
 
   return (
@@ -93,16 +154,13 @@ const ChatContainer = () => {
           />
         </Grid>
         <Grid item xs={1} align="right">
-          <Fab
-            color="primary"
-            aria-label="add"
+          <IconButton
             onClick={() => {
-              console.log("메시지 보내기");
-              // sendingMessage(ws, setSendMessage, sendMessage, token);
+              sendingMessage(ws, setSendMessage, sendMessage, token);
             }}
           >
             <SendIcon />
-          </Fab>
+          </IconButton>
         </Grid>
       </Grid>
     </Wrap>
