8442c9f98b2c015e7c1999bf026be133fbf4b77e
act99 <dorxm999@gmail.com>
2022-03-12 19:34:23 +0900

    okay

--- src/shared/url.js
+++ src/shared/url.js
@@ -1,10 +1,10 @@
 // ** 테스트
 const BASE_URL = "http://15.165.42.20:8080";
-const REDIRECT_URI = "http://localhost:3000/user/kakao/callback";
+// const REDIRECT_URI = "http://localhost:3000/user/kakao/callback";
 const WEB_SOCKET = "http://15.165.42.20:8080/ws-stomp";
 // ** 배포
 // const BASE_URL = "https://hanghae99.shop";
-// const REDIRECT_URI = "https://act99.shop";
+const REDIRECT_URI = "https://act99.shop";
 // const WEB_SOCKET = "https://hanghae99.shop/wss-stomp";
 
 const url = {
