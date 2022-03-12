8279cd43cdbe4845a86662c7853fdb1893dd8eda
act99 <dorxm999@gmail.com>
2022-03-12 19:35:56 +0900

    endpoint change

--- src/shared/url.js
+++ src/shared/url.js
@@ -1,11 +1,11 @@
 // ** 테스트
-const BASE_URL = "http://15.165.42.20:8080";
+// const BASE_URL = "http://15.165.42.20:8080";
 // const REDIRECT_URI = "http://localhost:3000/user/kakao/callback";
-const WEB_SOCKET = "http://15.165.42.20:8080/ws-stomp";
+// const WEB_SOCKET = "http://15.165.42.20:8080/ws-stomp";
 // ** 배포
-// const BASE_URL = "https://hanghae99.shop";
+const BASE_URL = "https://hanghae99.shop";
 const REDIRECT_URI = "https://act99.shop";
-// const WEB_SOCKET = "https://hanghae99.shop/wss-stomp";
+const WEB_SOCKET = "https://hanghae99.shop/ws-stomp";
 
 const url = {
   BASE_URL,
