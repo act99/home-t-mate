93997327400078711e1fda4dd715998083e1cad6
act99 <dorxm999@gmail.com>
2022-03-19 21:57:53 +0900

    이메일 제거

--- src/shared/OAuth.js
+++ src/shared/OAuth.js
@@ -6,7 +6,7 @@ const CLIENT_ID = url.CLIENT_KEY;
 const REDIRECT_URI = url.REDIRECT_URI;
 
 // 배포용
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=account_email`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 // 테스트용
 // export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
